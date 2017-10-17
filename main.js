795cbeb60f629b6af48acf76bbb9b20967af5fb1
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2017-10-17 04:02:43 +0000

    Added vertex texture cache repo

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 let port = 8081;
 
 // Serve static files
-app.use(express.static('public'))
-app.use(express.static('public/html'))
+app.use(express.static('public'));
+app.use(express.static('public/html'));
+
+// Public path for nested repositories
+app.use(express.static(path.join(__dirname, 'repos', 'vertex-texture-cache', 'public')));
+
+// Routes for nested repositories
+app.get('/vertexcache', (req, res) => {
+  // Vertex texture cache demo route
+  res.sendFile(path.join(__dirname, 'repos', 'vertex-texture-cache', 'public', 'html', 'index.html'));
+})
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
