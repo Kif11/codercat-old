53e9c7382526fa152a61f729bff5eeefe0f8e1e7
Kirill Kovalevskiy <kovalewskiy@gmail.com>
2017-12-11 21:12:26 +0000

    Added 360-photo-to-video

--- main.js
+++ main.js
@@ -10,11 +10,17 @@ app.use(express.static('public/html'));
 
 // Public path for nested repositories
 app.use(express.static(path.join(__dirname, 'repos', 'vertex-texture-cache', 'public')));
+app.use(express.static(path.join(__dirname, 'repos', '360-photo-to-video', 'public')));
 
 // Routes for nested repositories
 app.get('/vertexcache', (req, res) => {
   // Vertex texture cache demo route
   res.sendFile(path.join(__dirname, 'repos', 'vertex-texture-cache', 'public', 'html', 'index.html'));
+})
+
+app.get('/360-photo-to-video', (req, res) => {
+  // Vertex texture cache demo route
+  res.sendFile(path.join(__dirname, 'repos', '360-photo-to-video', 'public', 'html', 'index.html'));
 })
 
 app.listen(port, function () {
